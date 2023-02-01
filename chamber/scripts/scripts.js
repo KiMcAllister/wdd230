016542dbbfd186abcac9b5e52e32146d663255c4
KiMcAllister <97648593+KiMcAllister@users.noreply.github.com>
2023-01-31 19:09:11 -0700

    fix

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,10 +1,14 @@
-const footer1 = document.querySelectorAll("footer p")[0];
-const footer2 = document.querySelectorAll("footer p")[1];
+const footer1 = document.getElementById("page_info");
 
 const year = new Date().getFullYear();
 const lastUpdate = document.lastModified;
-footer1.innerHTML = `&copy; ${year} .:|:. Ki McAllister .:|:. UTAH`;
-footer2.innerHTML = `Last Updated: ${lastUpdate}`;
+footer1.innerHTML = footer1.innerHTML + `<p>&copy; ${year} Nebo Chamber</p><p>Ki McAllister</p><p>WDD 230 Project</p><p>Last Updated: ${lastUpdate}</p>`;
 
 const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(new Date());
 document.querySelector(".date").innerHTML = fulldate;
+
+document.getElementById("menu_btn").onclick = menu_toggle;
+function menu_toggle(){
+    console.log("working");
+    document.getElementById("header_nav").classList.toggle("open");
+}
