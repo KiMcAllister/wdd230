450afd820f250ac595b6e26ff2e85d68cbdd6444
KiMcAllister <97648593+KiMcAllister@users.noreply.github.com>
2023-03-11 15:34:23 -0700

    fix

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,15 +1,21 @@
+const apiKey = 'e528d9e6a0e372f8a07a6e790f6a1f37';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Nephi&appid=${apiKey}&units=imperial`;
 
-function calculate(){
-    console.log("working");
-    const t = 20;
-    const s = 10;
-    if ( t <= 50 && s > 3){
-        document.querySelector(".weather h3").innerHTML = (35.74 + 0.6215*t - 35.75 * (s ** 0.16) + 0.4275 * t * (s ** 0.16)).toFixed(2);
-    }
-    else{
-        document.querySelector(".weather h3").innerHTML = "N/A, wind speed isnt defined for tempertures above 50 degrees or wind speeds below 3 mph.";
-    }
+// Fetch the weather data
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Update the weather information in the HTML
+        document.querySelector('#temp').textContent = data.main.temp;
+        document.querySelector('#condition').textContent = data.weather[0].description;
+        document.querySelector('#icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`);
+        document.querySelector('#wind-speed').textContent = data.wind.speed;
+        const windChill = calculateWindChill(data.main.temp, data.wind.speed);
+        document.querySelector('#wind-chill').textContent = isNaN(windChill) ? 'N/A' : windChill.toFixed(1) + " ℉";
+    });
+
+// Calculate the wind chill using the current temperature and wind speed
+function calculateWindChill(temp, speed) {
+    const windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    return (temp <= 50 && speed >= 3) ? windChill : NaN;
 }
-
-calculate();
-
